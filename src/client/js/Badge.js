9bcfef499310fde46e0edf31377c79f4a38af71c
Marcel Goethals <marcel.goethals@gmail.com>
2016-06-30 17:40:06 +0200

    badge only second stage

--- src/client/js/Badge.js
+++ src/client/js/Badge.js
@@ -5,7 +5,7 @@ var Badge = {
     view: function(ctrl, length){
         var image = 1;
         if(length > 0) { image = 2; }
-        if(length > 2) { image = 3; }
+        //if(length > 2) { image = 3; }
 
         return m("img", {src:"static/stage"+image+".png"});
     }
